8474cebd8bfda0d6b7cc082e279e0024f50c075c
Paul Balanche <pb@buzzbrothers.ch>
2022-07-13 11:00:07 +0200

    first commit

---
+++ wpe.rb
@@ -0,0 +1,16 @@
+class wpe < Formula
+    desc ""
+    homepage ""
+    url "https://github.com/PaulBalanche/homebrew-wpe/archive/refs/heads/master.zip"
+    sha256 "7afb04fe3ae9233e57713652e6ff92b633fc40eab37b8090218eec3d52fcf2a7"
+    license ""
+  
+    def install
+      system "./configure", *std_configure_args, "--disable-silent-rules"
+    end
+  
+    test do
+      system "false"
+    end
+  end
+  
