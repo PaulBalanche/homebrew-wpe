fbddbdf7e3e217021c8c4246b81c118efb0d03b7
Paul Balanche <pb@buzzbrothers.ch>
2022-07-13 17:06:36 +0200

    Up

--- wpextend-cli.rb
+++ wpextend-cli.rb
@@ -1,8 +1,8 @@
 class WpextendCli < Formula
   desc "WP Extend CLI"
   homepage "https://github.com/PaulBalanche/wpextend-cli"
-  url "https://github.com/PaulBalanche/wpextend-cli/archive/refs/tags/0.0.1.tar.gz"
-  sha256 "2383c6e011cda40b13e7069d7039d9814e598759968f3be201b962adcb199d3c"
+  url "https://github.com/PaulBalanche/wpextend-cli/archive/refs/tags/0.0.2.tar.gz"
+  sha256 "f52924422b83dcfdc0a67920660597a91e83d45a6dc828c3a7203b577df62419"
   license "MIT"
   
     def install
