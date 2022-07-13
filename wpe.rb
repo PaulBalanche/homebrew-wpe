8a0c4813658ae9a536f30100dbdf992f8c624ae3
Paul Balanche <pb@buzzbrothers.ch>
2022-07-13 11:30:34 +0200

    Up version

--- wpe.rb
+++ wpe.rb
@@ -1,9 +1,10 @@
 class Wpe < Formula
     desc ""
-    homepage ""
-    url "https://github.com/PaulBalanche/homebrew-wpe/archive/refs/heads/master.zip"
+    homepage "https://github.com/PaulBalanche/homebrew-wpe"
+    url "https://github.com/PaulBalanche/homebrew-wpe/archive/refs/tags/0.0.5.tar.gz"
     sha256 "7afb04fe3ae9233e57713652e6ff92b633fc40eab37b8090218eec3d52fcf2a7"
-    license ""
+    license "MIT"
+    version "0.0.5"
   
     def install
       system "./configure", *std_configure_args, "--disable-silent-rules"
