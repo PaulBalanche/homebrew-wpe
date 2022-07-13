b7bc68448427f0572c04a267eaad7b8592f1e3f1
Paul Balanche <pb@buzzbrothers.ch>
2022-07-13 17:34:29 +0200

    Up

--- wpextend-cli.rb
+++ wpextend-cli.rb
@@ -7,8 +7,8 @@ class WpextendCli < Formula
   
     def install
       bin.install 'wpextend.sh'
-      bin.install Dir["include"]    
-      bin.install Dir["docker"]
+      install Dir["include"]    
+      install Dir["docker"]
     end
   end
   
