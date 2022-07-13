1b27d432031f8779d206018b38603d0a85612946
Paul Balanche <pb@buzzbrothers.ch>
2022-07-13 14:05:52 +0200

    Version 1

--- wpextend-cli.rb
+++ wpextend-cli.rb
@@ -1,10 +1,9 @@
 class WpextendCli < Formula
-    desc ""
-    homepage "https://github.com/PaulBalanche/homebrew-wpextend-cli"
-    url "https://github.com/PaulBalanche/homebrew-wpextend-cli/archive/refs/tags/0.0.7.tar.gz"
-    sha256 "2ef232d6bf722db1d74ae9303e1a57718463ff3b3c0e240398e03ef29fd161e9"
-    license "MIT"
-    version "0.0.7"
+  desc "WP Extend CLI"
+  homepage "https://github.com/PaulBalanche/wpextend-cli"
+  url "https://github.com/PaulBalanche/wpextend-cli/archive/refs/tags/0.0.1.tar.gz"
+  sha256 "2383c6e011cda40b13e7069d7039d9814e598759968f3be201b962adcb199d3c"
+  license "MIT"
   
     def install
       system "./configure", *std_configure_args, "--disable-silent-rules"
